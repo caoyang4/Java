4dbd4833a103b7e0ad3dbb1fd17c544b85398db7
caoyang42 <caoyang42@meituan.com>
2022-01-21 17:39:19 +0800

    java入坑之路

---
+++ src/algorithm/leetcode/Leetcode1.java
@@ -0,0 +1,47 @@
+package src.algorithm.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 两数之和
+ * 给定一个整数数组 nums 和一个整数目标值 target，
+ * 请你在该数组中找出 和为目标值 target  的两个整数，并返回它们的数组下标。
+ * @author caoyang
+ */
+public class Leetcode1 {
+    /**
+     * O(N^2)
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        int [] res = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target){
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        int [] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])){
+                res[0] = map.get(nums[i]);
+                res[1] = i;
+                return res;
+            }
+            map.put(target-nums[i], i);
+        }
+
+        return res;
+    }
+
+
+
+}
---
+++ src/algorithm/leetcode/Leetcode2.java
@@ -0,0 +1,62 @@
+package src.algorithm.leetcode;
+
+import java.util.List;
+
+/**
+ * 两数相加
+ *
+ * 给你两个非空的链表，表示两个非负的整数。它们每位数字都是逆序的方式存储的，并且每个节点只能存储一位数字。
+ * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ *
+ * @author caoyang
+ */
+public class Leetcode2 {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        ListNode resNode = new ListNode(0);
+        ListNode cursor = resNode;
+        int carry = 0;
+        /*
+        * 链表不为空或者存在进位
+        */
+        while (l1 != null || l2 != null || carry != 0){
+            int v1 = l1 != null ? l1.val : 0;
+            int v2 = l2 != null ? l2.val : 0;
+            int sum = v1 + v2 + carry;
+            carry = sum / 10;
+            ListNode node = new ListNode(sum % 10);
+            cursor.next = node;
+            // cursor游标向前移动
+            cursor = node;
+
+            if (l1 != null){ l1 = l1.next; }
+            if (l2 != null){ l2 = l2.next; }
+        }
+        return resNode.next;
+    }
+
+    public static void printNode(ListNode l){
+        while (l != null){
+            System.out.print(l.val + "\t");
+            l = l.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(5, new ListNode(9));
+        ListNode l2 = new ListNode(6);
+        ListNode resNode = addTwoNumbers(l1, l2);
+
+        printNode(resNode);
+
+    }
+
+
+}
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
