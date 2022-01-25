858f08eb86474c234b3e2924f6b544e477506129
caoyang42 <caoyang42@meituan.com>
2022-01-25 18:48:45 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/socket/udp/Client.java
+++ src/socket/udp/Client.java
@@ -1,7 +1,29 @@
 package src.socket.udp;
 
+import java.io.IOException;
+import java.net.*;
+
 /**
  * @author caoyang
  */
 public class Client {
+    public static void main(String[] args) throws IOException {
+        String msg = "user:young password:admin123";
+        byte[] data = msg.getBytes();
+        InetAddress address = InetAddress.getByName("localhost");
+        // 数据包
+        DatagramPacket datagramPacket = new DatagramPacket(data, data.length, address, 8000);
+        DatagramSocket socket = new DatagramSocket();
+        System.out.println("客户端发送消息：" + msg);
+        socket.send(datagramPacket);
+
+        byte[] info = new byte[1024];
+        DatagramPacket datagramPacket1 = new DatagramPacket(info, info.length);
+        socket.receive(datagramPacket1);
+        String rec  = new String(datagramPacket1.getData(), 0, datagramPacket1.getLength());
+        System.out.println("客户端获取消息："+rec);
+
+        socket.close();
+
+    }
 }
--- src/socket/udp/Server.java
+++ src/socket/udp/Server.java
@@ -1,7 +1,38 @@
 package src.socket.udp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 /**
  * @author caoyang
  */
 public class Server {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket datagramSocket = new DatagramSocket(8000);
+        byte[] data = new byte[1024];
+        DatagramPacket datagramPacket = new DatagramPacket(data, data.length);
+        System.out.println("服务端启动...");
+
+        // 接收数据
+        datagramSocket.receive(datagramPacket);
+        String msg = new String(datagramPacket.getData(), 0, datagramPacket.getLength());
+        System.out.println("服务端获取消息：" + msg);
+
+        // 发送数据
+        String info = "welcome...";
+        byte[] send_data = info.getBytes();
+        InetAddress address = datagramPacket.getAddress();
+        int port = datagramPacket.getPort();
+        DatagramPacket datagramPacket1 = new DatagramPacket(send_data, send_data.length, address, port);
+        datagramSocket.send(datagramPacket1);
+        System.out.println("服务端发送消息：" + info);
+
+        // 关闭 socket
+        datagramSocket.close();
+
+
+    }
 }
