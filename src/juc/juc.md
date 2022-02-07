99c906c2a2f2041f7634161f790dc8651a3d4978
caoyang42 <caoyang42@meituan.com>
2022-02-07 11:02:37 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/juc/juc.md
+++ src/juc/juc.md
@@ -1,1 +1,5 @@
 高并发
+
+threadLocal 内存泄漏示意图
+
+![threadLocalOOM.png](threadLocalOOM.png)
---
+++ src/juc/thread/threadLocalOOM/MyThreadLocalOOM.java
@@ -0,0 +1,58 @@
+package src.juc.thread.threadLocalOOM;
+
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class MyThreadLocalOOM {
+    public static final Integer SIZE = 500;
+    static ThreadPoolExecutor executor = new ThreadPoolExecutor(
+            5, 5, 1,
+            TimeUnit.MINUTES, new LinkedBlockingDeque<>());
+
+    static class LocalVariable {//总共有5M
+        private byte[] bytes = new byte[1024 * 1024 * 5];
+    }
+
+    static ThreadLocal<LocalVariable> local = new ThreadLocal<>();
+
+    public static void revealTest(){
+        try {
+            for (int i = 0; i < SIZE; i++) {
+                executor.execute(() -> {
+                    local.set(new LocalVariable());
+                    System.out.println("开始执行");
+                });
+                Thread.sleep(100);
+            }
+            // local设置为null，依旧会造成内存泄漏
+            local = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void recycleTest(){
+        try {
+            for (int i = 0; i < SIZE; i++) {
+                executor.execute(() -> {
+                    local.set(new LocalVariable());
+                    System.out.println("开始执行");
+                    // 调用local.remove()将threadLocal内的对象删除
+                    local.remove();
+                });
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // 泄漏
+//        revealTest();
+
+        // 回收
+         recycleTest();
+    }
+}
