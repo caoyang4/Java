7037c7c5f58dbfc5d3941c1aa7884351a6ca3ba7
caoyang42 <caoyang42@meituan.com>
2022-02-21 20:13:20 +0800

    欢迎入坑Java，开启从入门到放弃之路

---
+++ src/juc/thread/TestReorder.java
@@ -0,0 +1,52 @@
+package src.juc.thread;
+
+/**
+ * 指令重排序验证
+ * @author caoyang
+ */
+public class TestReorder {
+    private  static int x =0, y =0;
+    private  static int a =0, b =0;
+
+    public static void main(String[] args) throws InterruptedException {
+        int i =0;
+
+        for (;;){
+            i++;
+            x =0; y =0;
+            a =0; b =0;
+            Thread t1 =new Thread(() -> {
+                shortWait(20000);
+                a =1;
+                x =b;
+            });
+
+            Thread t2 =new Thread(() -> {
+                b =1;
+                y =a;
+            });
+
+            t1.start();
+            t2.start();
+            t1.join();
+            t2.join();
+
+            String result ="第" + i +"次 (" +x +"," +y +"）";
+            if(x == 0 && y == 0) {
+                System.out.println(result + "，发生指令重排序！");
+                break;
+            }else {
+                System.out.println(result);
+            }
+        }
+    }
+    public static void shortWait(long interval){
+        long start = System.nanoTime();
+        long end;
+        do{
+            end = System.nanoTime();
+        }while(start + interval >= end);
+    }
+
+}
+
