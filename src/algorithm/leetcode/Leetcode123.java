9b5f899eddbc120b13de4f3a0c530b4ccdbefc52
caoyang42 <caoyang42@meituan.com>
2022-02-21 10:47:24 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode123.java
+++ src/algorithm/leetcode/Leetcode123.java
@@ -3,7 +3,7 @@ package src.algorithm.leetcode;
 /**
  * 给定一个数组，它的第 i 个元素是一支给定的股票在第 i 天的价格。
  * 设计一个算法来计算你所能获取的最大利润。你最多可以完成两笔交易。
- * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * 你不能同时参与多笔交易，即必须卖光后才能买入，但可以同一天卖完后，再买入
  *
  *输入：prices = [3,3,5,0,0,3,1,4]
  * 输出：6
@@ -13,5 +13,53 @@ package src.algorithm.leetcode;
  * @author caoyang
  */
 public class Leetcode123 {
+    /**
+     * 分为 5 个阶段
+     * 阶段一、第一次买之前
+     * 阶段二、持有股票
+     * 阶段三、第一次卖之后，第二次买之前
+     * 阶段四、持有股票
+     * 阶段五、第二次卖之后
+     *
+     * @param prices
+     * @return
+     */
+    public static int maxProfit(int[] prices) {
+        if(prices == null && prices.length == 1){
+            return 0;
+        }
+        int[][] f = new int[prices.length+1][5+1];
+        // 前 0 天
+        // 阶段一
+        f[0][1] = 0;
+        f[0][2] = f[0][3] = f[0][4] = f[0][5] = Integer.MIN_VALUE;
+        for (int i = 1; i <= prices.length; i++) {
+            // 一三五阶段，手中无股票
+            // f[i][j] = max{f[i-1][j], f[i-1][j-1] + p[i-1] - p[i-2]}
+            for (int j = 1; j <= 5; j += 2) {
+                f[i][j] = f[i-1][j];
+                if(i >= 2 && j >= 2 && f[i-1][j-1] != Integer.MIN_VALUE){
+                    f[i][j] = Math.max(f[i][j], f[i-1][j-1] + prices[i-1] - prices[i-2]);
+                }
+            }
+            // 二四阶段，手中持有股票
+            // f[i][j] = max{f[i-1][j] + p[i-1] - p[i-2], f[i-1][j-1], f[i-1][j-2] + p[i-1] - p[i-2] }
+            for (int j = 2; j <= 4; j+=2) {
+                f[i][j] = f[i-1][j-1];
+                if(i > 1 && f[i-1][j] != Integer.MIN_VALUE){
+                    f[i][j] = Math.max(f[i][j], f[i-1][j] + prices[i-1] - prices[i-2]);
+                }
+                if(i > 1 && j > 2 && f[i-1][j-2] != Integer.MIN_VALUE){
+                    f[i][j] = Math.max(f[i][j], f[i-1][j-2] + prices[i-1] - prices[i-2]);
+                }
+            }
+        }
+        return Math.max(Math.max(f[prices.length][1], f[prices.length][3]),f[prices.length][5]);
+    }
 
+    public static void main(String[] args) {
+        int[] prices = {3,3,5,0,0,3,1,4};
+        int res = maxProfit(prices);
+        System.out.println(res);
+    }
 }
