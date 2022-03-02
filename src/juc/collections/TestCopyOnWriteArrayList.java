8da9ea985c6f5a5814bdf11172c75687f70b3b8e
caoyang42 <caoyang42@meituan.com>
2022-03-02 16:32:54 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/juc/collections/TestCopyOnWriteArrayList.java
+++ src/juc/collections/TestCopyOnWriteArrayList.java
@@ -1,27 +1,51 @@
 package src.juc.collections;
 
 import java.util.Iterator;
+import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author caoyang
  */
 public class TestCopyOnWriteArrayList {
     public static void main(String[] args) throws InterruptedException {
-        CopyOnWriteArrayList<Integer> list = new CopyOnWriteArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        // list弱一致性，后续其他线程的增删改不影响iter的遍历
-        Iterator<Integer> iter = list.iterator();
-        new Thread(() -> {
-            list.remove(0);
-            System.out.println(list);
-        }).start();
-        TimeUnit.SECONDS.sleep(1);
-        while (iter.hasNext()) {
-            System.out.println(iter.next());
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(UUID.randomUUID().toString().substring(0,8));
+        }
+        Iterator<String> iterator = list.iterator();
+        TestCopyOnWriteArrayListThread thread = new TestCopyOnWriteArrayListThread("ThreadCOW", list);
+        thread.start();
+        while (iterator.hasNext()){
+            System.out.print(iterator.next() + "\t");
+        }
+        System.out.println();
+        Thread.sleep(2000);
+        iterator = list.iterator();
+        while (iterator.hasNext()){
+            System.out.print(iterator.next() + "\t");
+        }
+
+    }
+}
+
+class TestCopyOnWriteArrayListThread extends Thread{
+    CopyOnWriteArrayList list;
+
+    public TestCopyOnWriteArrayListThread(String name, CopyOnWriteArrayList list) {
+        super(name);
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            list.add(UUID.randomUUID().toString().substring(0,8));
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
