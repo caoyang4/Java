617c358d504aee239ca0ed768511c92c95f38f45
caoyang42 <caoyang42@meituan.com>
2022-03-14 12:58:04 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode44.java
+++ src/algorithm/leetcode/Leetcode44.java
@@ -14,10 +14,39 @@ package src.algorithm.leetcode;
  */
 public class Leetcode44 {
     public static boolean isMatch(String s, String p) {
-        return true;
+        int m = s.length();
+        int n = p.length();
+        char[] chars1 = s.toCharArray();
+        char[] chars2 = p.toCharArray();
+        boolean[][] dp = new boolean[m+1][n+1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 && j == 0){
+                    dp[i][j] = true;
+                    continue;
+                }
+                if (j == 0){
+                    dp[i][j] = false;
+                    continue;
+                }
+                if (chars2[j-1] != '*'){
+                    if (i > 0 && (chars1[i-1] == chars2[j-1] || chars2[j-1] == '?')){
+                        dp[i][j] |= dp[i-1][j-1];
+                    }
+                } else {
+                    // 当 p[j-1]='*'，即 s[0:i-1] 与 p[0:j]是否匹配，或s[0:i] 与 p[0:j-1]是否匹配
+                    dp[i][j] |= dp[i][j-1];
+                    if (i > 0){ dp[i][j] |= dp[i-1][j]; }
+                }
+            }
+        }
+        return dp[m][n];
     }
 
     public static void main(String[] args) {
-
+        String s = "aa";
+        String p = "a";
+        boolean res = isMatch(s, p);
+        System.out.println(res);
     }
 }
