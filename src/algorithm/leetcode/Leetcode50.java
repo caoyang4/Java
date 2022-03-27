3a24cfa2bfbf8408f650f9cdf90034742d34a45f
caoyang42 <caoyang42@meituan.com>
2022-03-27 15:59:07 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode50.java
+++ src/algorithm/leetcode/Leetcode50.java
@@ -1,4 +1,60 @@
 package src.algorithm.leetcode;
 
+/**
+ * 50. Pow(x, n)
+ *
+ * 输入：x = 2.00000, n = 10
+ * 输出：1024.00000
+ * @author caoyang
+ */
 public class Leetcode50 {
+    public static double myPow1(double x, int n) {
+        if (Math.abs(x) == 1.0){
+            return n % 2 == 1 ? x : Math.abs(x);
+        }
+        if (n == 0){
+            return 1.0;
+        }
+        double result = x;
+        if(n > 0){
+            for (int i = 0; i < n - 1; i++) {
+                result *= x;
+            }
+        } else {
+            // Integer 溢出
+            if(n == Math.abs(n)){
+                return 0.0;
+            }
+            for (int i = n; i <= 0; i++) {
+                result /= x;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 使用折半计算，每次把n缩小一半，这样n最终会缩小到0，任何数的0次方都为1，这时候我们再往回乘，
+     * 如果此时n是偶数，直接把上次递归得到的值算个平方返回即可，如果是奇数，则还需要乘上个x的值
+     * @param x
+     * @param n
+     * @return
+     */
+    public static double myPow(double x, int n) {
+        double res = 1.0;
+        for(int i = n; i != 0; i /= 2){
+            if(i % 2 != 0){
+                res *= x;
+            }
+            x *= x;
+        }
+        return  n < 0 ? 1 / res : res;
+    }
+
+    public static void main(String[] args) {
+        double x = 2.10000;
+        int n = 3;
+        double result = myPow(x, n);
+        System.out.println(result);
+
+    }
 }
--- src/algorithm/leetcode/Leetcode51.java
+++ src/algorithm/leetcode/Leetcode51.java
@@ -1,4 +1,109 @@
 package src.algorithm.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 51. N 皇后
+ * @author caoyang
+ */
 public class Leetcode51 {
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        List<String> pieces = new ArrayList<>();
+        StringBuilder dots = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            dots.append(".");
+        }
+        for (int i = 0; i < n; i++) {
+            pieces.add(dots.toString());
+        }
+        trackBack(result, pieces, n, 0);
+        return result;
+    }
+
+    public static void trackBack(List<List<String>> result, List<String> pieces, int n, int start){
+        if (start == n){
+            result.add(new ArrayList<>(pieces));
+            return;
+        }
+        for (int j = 0; j < n; j++) {
+            if(pieces.get(start).charAt(j) == 'Q'){
+                // 棋盘 [i,j] 上已有皇后，跳出该列，找下一行
+                break;
+            }
+            if(isValid(pieces, n, start, j)){
+                String piece = fillQueens(pieces.get(start), j, "Q");
+                pieces.set(start, piece);
+                trackBack(result, pieces, n, start+1);
+                piece = fillQueens(pieces.get(start), j, ".");
+                pieces.set(start, piece);
+            }
+        }
+    }
+
+    public static String fillQueens(String piece, int j, String replacement){
+        StringBuilder tmp = new StringBuilder(piece);
+        tmp.replace(j, j+1, replacement);
+        return tmp.toString();
+    }
+
+    /**
+     * 不能同行
+     * 不能同列
+     * 不能同斜线
+     * @param pieces
+     * @param n
+     * @param row
+     * @param col
+     * @return
+     */
+    public static boolean isValid(List<String> pieces, int n, int row, int col){
+        for (int i = 0; i < n; i++) {
+            // 竖列不能有其他皇后
+            if (i != row && pieces.get(i).charAt(col) == 'Q'){
+                return false;
+            }
+            // 横排不能有其他皇后
+            if (i != col && pieces.get(row).charAt(i) == 'Q'){
+                return false;
+            }
+        }
+        // 十字斜线不能有其他皇后
+        int i = row, j = col;
+        while (i > 0 && j > 0){
+            if(pieces.get(--i).charAt(--j) == 'Q'){
+                return false;
+            }
+        }
+        i = row;
+        j = col;
+        while (i < n-1 && j > 0){
+            if(pieces.get(++i).charAt(--j) == 'Q'){
+                return false;
+            }
+        }
+        i = row;
+        j = col;
+        while (i > 0 && j < n-1){
+            if(pieces.get(--i).charAt(++j) == 'Q'){
+                return false;
+            }
+        }
+        i = row;
+        j = col;
+        while (i < n-1 && j < n-1){
+            if(pieces.get(++i).charAt(++j) == 'Q'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        List<List<String>> result = solveNQueens(4);
+        for (List<String> list : result) {
+            System.out.println(list);
+        }
+    }
 }
