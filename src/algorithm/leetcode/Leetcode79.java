331ec689b611ac14da61d8ed0e50226bb17886df
caoyang42 <caoyang42@meituan.com>
2022-03-30 23:08:24 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode79.java
+++ src/algorithm/leetcode/Leetcode79.java
@@ -1,4 +1,17 @@
 package src.algorithm.leetcode;
 
+/**
+ * 79. 单词搜索
+ *
+ * 给定一个m x n 二维字符网格board 和一个字符串单词word 。如果word存在于网格中，返回true；否则，返回 false 。
+ * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。
+ * 同一个单元格内的字母不允许被重复使用
+ *
+ *
+ * @author caoyang
+ */
 public class Leetcode79 {
+    public boolean exist(char[][] board, String word) {
+        return true;
+    }
 }
