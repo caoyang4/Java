edd735ba27e0a0c44756b28915466bc03aff37d8
caoyang42 <caoyang42@meituan.com>
2022-03-30 23:08:15 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode77.java
+++ src/algorithm/leetcode/Leetcode77.java
@@ -1,4 +1,57 @@
 package src.algorithm.leetcode;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 77. 组合
+ *
+ * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+ * 你可以按 任何顺序 返回答案
+ *
+ * 输入：n = 1, k = 1
+ * 输出：[[1]]
+ * @author caoyang
+ */
 public class Leetcode77 {
+    public static List<List<Integer>> combine(int n, int k) {
+        if (k > n){
+            return new ArrayList<>();
+        }
+        int[] nums = new int[n];
+        boolean[] used = new boolean[n];
+        for (int i = 1; i <= n; i++) {
+            nums[i-1] = i;
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        // 搜搜深度和起始点不一样
+        trackBack(result, new LinkedList<>(), nums, used, k, 0, 0);
+        return result;
+    }
+    public static void trackBack(List<List<Integer>> result, Deque<Integer> path, int[] nums, boolean[] used, int k, int depth, int start){
+        if(depth == k){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            if(!used[i]){
+                path.add(nums[i]);
+                used[i] = true;
+                trackBack(result, path, nums, used, k, depth+1, i+1);
+                used[i] = false;
+                path.removeLast();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int k = 2;
+        List<List<Integer>> result = combine(n, k);
+        for (List<Integer> integers : result) {
+            System.out.println(integers);
+        }
+    }
 }
--- src/algorithm/leetcode/Leetcode78.java
+++ src/algorithm/leetcode/Leetcode78.java
@@ -1,4 +1,53 @@
 package src.algorithm.leetcode;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * 78. 子集
+ * 给你一个整数数组 nums ，数组中的元素 互不相同 。
+ * 返回该数组所有可能的子集（幂集）
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ *
+ * @author caoyang
+ */
 public class Leetcode78 {
+    public static List<List<Integer>> subsets(int[] nums) {
+        int n = nums.length;
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        result.add(Arrays.stream(nums).boxed().collect(Collectors.toList()));
+        boolean[] used = new boolean[n];
+        for (int i = 1; i < n; i++) {
+            List<List<Integer>> iRes = new ArrayList<>();
+            trackBack(iRes, new LinkedList<>(), nums, used, i, 0, 0);
+            result.addAll(iRes);
+        }
+        return result;
+    }
+    public static void trackBack(List<List<Integer>> result, Deque<Integer> path, int[] nums, boolean[] used, int k, int depth, int start){
+        if(depth == k){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = start; i < nums.length; i++) {
+            if(!used[i]){
+                path.add(nums[i]);
+                used[i] = true;
+                trackBack(result, path, nums, used, k, depth+1, i+1);
+                used[i] = false;
+                path.removeLast();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        List<List<Integer>> result = subsets(nums);
+        for (List<Integer> integers : result) {
+            System.out.println(integers);
+        }
+    }
 }
