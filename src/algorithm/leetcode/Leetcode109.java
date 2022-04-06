3a3cd497e6d154582357f62e06b69ee5163c4f46
caoyang42 <caoyang42@meituan.com>
2022-04-06 20:15:05 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode109.java
+++ src/algorithm/leetcode/Leetcode109.java
@@ -1,5 +1,8 @@
 package src.algorithm.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 109. 有序链表转换二叉搜索树
  * 给定一个单链表的头节点head，其中的元素按升序排序 ，将其转换为高度平衡的二叉搜索树
@@ -7,8 +10,26 @@ package src.algorithm.leetcode;
  */
 public class Leetcode109 {
     public TreeNode sortedListToBST(ListNode head) {
+        if (head == null){
+            return null;
+        }
+        List<Integer> list = new ArrayList<>();
+        while (head != null){
+            list.add(head.val);
+            head = head.next;
+        }
+        return sortedListToBST(list, 0, list.size());
+    }
+    public TreeNode sortedListToBST(List<Integer> list, int start, int end){
+        if(start == end){
+            return null;
+        }
+        int middle = start + ((end - start) >> 1);
+        TreeNode root = new TreeNode(list.get(middle));
 
-        return null;
+        root.left = sortedListToBST(list,start, middle);
+        root.right = sortedListToBST(list, middle+1, end);
+        return root;
     }
 
     public static void main(String[] args) {
--- src/algorithm/leetcode/Leetcode17.java
+++ src/algorithm/leetcode/Leetcode17.java
@@ -16,52 +16,37 @@ import java.util.List;
  * @author caoyang
  */
 public class Leetcode17 {
-    /**
-     * 设置全局列表存储最后的结果
-     */
-    List<String> list = new ArrayList<>();
 
-    public List<String> letterCombinations(String digits) {
+    public static List<String> letterCombinations(String digits) {
+        List<String> list = new ArrayList<>();
         if (digits == null || digits.length() == 0) {
             return list;
         }
         //初始对应所有的数字，为了直接对应2-9，新增了两个无效的字符串""
         String[] numString = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        //迭代处理
-        backTracking(digits, numString, 0);
+        backTrack(list, new StringBuilder(), digits, numString, 0);
         return list;
-
     }
 
-    /**
-     * 每次迭代获取一个字符串，所以会设计大量的字符串拼接，
-     * 所以这里选择更为高效的 StringBuilder
-     */
-    StringBuilder temp = new StringBuilder();
 
-
-    public void backTracking(String digits, String[] numString, int num) {
-        //遍历全部一次记录一次得到的字符串
-        if (num == digits.length()) {
-            list.add(temp.toString());
+    public static void backTrack(List<String> list, StringBuilder tmp, String digits, String[] numString, int start) {
+        if(start == digits.length()){
+            list.add(tmp.toString());
             return;
         }
-        //str 表示当前num对应的字符串
-        String str = numString[digits.charAt(num) - '0'];
-        for (int i = 0; i < str.length(); i++) {
-            temp.append(str.charAt(i));
-            // 递归
-            backTracking(digits, numString, num + 1);
-            //剔除末尾的继续尝试
-            temp.deleteCharAt(temp.length() - 1);
+        char[] alphas = numString[digits.charAt(start)-'0'].toCharArray();
+        for (char alpha : alphas) {
+            tmp.append(alpha);
+            backTrack(list, tmp, digits, numString, start+1);
+            tmp.delete(tmp.length()-1, tmp.length());
         }
     }
+
     public static void main(String[] args) {
         String digits = "23";
-        List<String> res = new Leetcode17().letterCombinations(digits);
+        List<String> res = letterCombinations(digits);
         for (String re : res) {
             System.out.println(re);
         }
-
     }
 }
