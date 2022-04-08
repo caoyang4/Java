d66b21bf45171747bfc45d5ef5f94935c4694e97
caoyang42 <caoyang42@meituan.com>
2022-04-08 11:28:29 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode120.java
+++ src/algorithm/leetcode/Leetcode120.java
@@ -1,9 +1,13 @@
 package src.algorithm.leetcode;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 120. 三角形最小路径和
- *
  * 给定一个三角形 triangle ，找出自顶向下的最小路径和。
+ * 每一步只能移动到下一行中相邻的结点上。相邻的结点在这里指的是下标与上一层结点下标相同或者等于上一层结点下标+1的两个结点。
+ * 也就是说，如果正位于当前行的下标 i ，那么下一步可以移动到下一行的下标 i 或 i + 1
  *
  * 输入：triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
  * 输出：11
@@ -11,4 +15,29 @@ package src.algorithm.leetcode;
  * @author caoyang
  */
 public class Leetcode120 {
+    public static int minimumTotal(List<List<Integer>> triangle) {
+        if (triangle.isEmpty() || triangle.get(0).isEmpty()){
+            return 0;
+        }
+        int len = triangle.size();
+        int[] dp = new int[len + 1];
+        for (int i = len-1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = Math.min(dp[j], dp[j+1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
+
+
+    public static void main(String[] args) {
+        List<List<Integer>> triangle = Arrays.asList(
+                Arrays.asList(2),
+                Arrays.asList(3,4),
+                Arrays.asList(6,5,7),
+                Arrays.asList(4,1,8,3)
+        );
+        int result = minimumTotal(triangle);
+        System.out.println(result);
+    }
 }
--- src/algorithm/leetcode/Leetcode124.java
+++ src/algorithm/leetcode/Leetcode124.java
@@ -1,7 +1,22 @@
 package src.algorithm.leetcode;
 
 /**
+ * 124. 二叉树中的最大路径和
+ * 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。同一个节点在一条路径序列中 至多出现一次 。
+ * 该路径至少包含一个节点，且不一定经过根节点。
+ * 路径和是路径中各节点值的总和。
+ *
+ * 输入：root = [-10,9,20,null,null,15,7]
+ * 输出：42
+ *
  * @author caoyang
  */
 public class Leetcode124 {
+    public int maxPathSum(TreeNode root) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
