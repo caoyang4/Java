9fef70535bac5bc1e9b84b92a85b52e690589f1e
caoyang42 <caoyang42@meituan.com>
2022-04-20 12:47:42 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode173.java
+++ src/algorithm/leetcode/Leetcode173.java
@@ -1,7 +1,43 @@
 package src.algorithm.leetcode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
+ * 173. 二叉搜索树迭代器
+ * 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器
  * @author caoyang
  */
 public class Leetcode173 {
+
+    class BSTIterator {
+        TreeNode root;
+        Deque<TreeNode> deque;
+        public BSTIterator(TreeNode root) {
+            this.root = root;
+            this.deque = new LinkedList<>();
+            traverse(root);
+        }
+
+        public void traverse(TreeNode root){
+            if (root == null){
+                return;
+            }
+            traverse(root.left);
+            this.deque.add(root);
+            traverse(root.right);
+        }
+
+        public int next() {
+            return this.deque.pop().val;
+        }
+
+        public boolean hasNext() {
+            return !this.deque.isEmpty();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/algorithm/leetcode/Leetcode179.java
+++ src/algorithm/leetcode/Leetcode179.java
@@ -1,7 +1,27 @@
 package src.algorithm.leetcode;
 
+import java.util.Arrays;
+
 /**
+ * 179. 最大数
+ * 输入：nums = [3,30,34,5,9]
+ * 输出："9534330"
  * @author caoyang
  */
 public class Leetcode179 {
+    public static String largestNumber(int[] nums) {
+        String[] strings = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strings[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(strings, (a, b) -> (b+a).compareTo(a+b));
+        if("0".equals(strings[0])){ return "0";}
+        return String.join("", strings);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {111311, 1113};
+        String result = largestNumber(nums);
+        System.out.println(result);
+    }
 }
