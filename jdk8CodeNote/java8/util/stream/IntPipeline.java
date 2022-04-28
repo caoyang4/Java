c84622c74f28be00eb85d24451ab0b40b4049061
caoyang42 <caoyang42@meituan.com>
2022-04-28 16:35:50 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- jdk8CodeNote/java8/util/stream/IntPipeline.java
+++ jdk8CodeNote/java8/util/stream/IntPipeline.java
@@ -50,9 +50,7 @@ import java.util.function.Supplier;
  * @param <E_IN> type of elements in the upstream source
  * @since 1.8
  */
-abstract class IntPipeline<E_IN>
-        extends AbstractPipeline<E_IN, Integer, IntStream>
-        implements IntStream {
+abstract class IntPipeline<E_IN> extends AbstractPipeline<E_IN, Integer, IntStream> implements IntStream {
 
     /**
      * Constructor for the head of a stream pipeline.
