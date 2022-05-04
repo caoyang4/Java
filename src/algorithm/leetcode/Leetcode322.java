e72ad537193b13ab77738d3e42478e976d4bfb25
caoyang42 <caoyang42@meituan.com>
2022-05-04 13:24:44 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode322.java
+++ src/algorithm/leetcode/Leetcode322.java
@@ -1,7 +1,38 @@
 package src.algorithm.leetcode;
 
 /**
+ * 322. 零钱兑换
+ *
+ * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+ * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回-1 。
+ * 你可以认为每种硬币的数量是无限的。
+ *
+ * 输入：coins = [1, 2, 5], amount = 11
+ * 输出：3
+ * 解释：11 = 5 + 5 + 1
+ *
  * @author caoyang
  */
 public class Leetcode322 {
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount+1];
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int coin : coins) {
+                if(i - coin >= 0 && dp[i-coin] != Integer.MAX_VALUE){
+                    dp[i] = Math.min(dp[i], dp[i-coin]+1);
+                }
+            }
+
+        }
+        return dp[amount] != Integer.MAX_VALUE ? dp[amount] : -1;
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        int result = coinChange(coins, amount);
+        System.out.println(result);
+    }
 }
--- src/algorithm/leetcode/Leetcode328.java
+++ src/algorithm/leetcode/Leetcode328.java
@@ -1,7 +1,37 @@
 package src.algorithm.leetcode;
 
 /**
+ * 328. 奇偶链表
+ * 给定单链表的头节点head，将所有索引为奇数的节点和索引为偶数的节点分别组合在一起，然后返回重新排序的列表。
+ * 第一个节点的索引被认为是奇数 ， 第二个节点的索引为偶数
+ *
+ * 输入: head = [1,2,3,4,5]
+ * 输出: [1,3,5,2,4]
  * @author caoyang
  */
 public class Leetcode328 {
+    public ListNode oddEvenList(ListNode head) {
+        ListNode odd = new ListNode();
+        ListNode oddCursor = odd;
+        ListNode even = new ListNode();
+        ListNode evenCursor = even;
+        int index = 0;
+        while (head != null){
+            if(index++ % 2 == 0){
+                oddCursor.next = head;
+                oddCursor = oddCursor.next;
+            } else {
+                evenCursor.next = head;
+                evenCursor = evenCursor.next;
+            }
+            head = head.next;
+        }
+        oddCursor.next = even.next;
+        evenCursor.next = null;
+        return odd.next;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
