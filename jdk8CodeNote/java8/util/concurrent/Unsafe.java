71f2bd65dd57516ae1e1cef36c9ccfea570b3d52
caoyang42 <caoyang42@meituan.com>
2022-05-04 13:45:21 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- jdk8CodeNote/java8/util/concurrent/Unsafe.java
+++ jdk8CodeNote/java8/util/concurrent/Unsafe.java
@@ -1,12 +1,17 @@
 
 package sun.misc;
 
+/**
+ * Java中的Unsafe类为我们提供了管理内存的能力
+ * Unsafe类是final的，不允许继承，而且构造函数是私有的，无法通过new的方式创建对象
+ * 但可以通过反射创建
+ */
+
 import java.security.*;
 import java.lang.reflect.*;
 
 import sun.reflect.CallerSensitive;
 import sun.reflect.Reflection;
-
 
 
 public final class Unsafe {
@@ -19,6 +24,9 @@ public final class Unsafe {
 
     private Unsafe() {}
 
+    /**
+     * 饿汉式单例模式
+     */
     private static final Unsafe theUnsafe = new Unsafe();
 
     @CallerSensitive
@@ -29,187 +37,109 @@ public final class Unsafe {
         return theUnsafe;
     }
 
+    /**
+     * 普通读写，无法保证可见性和有序性
+     * 可以通过Unsafe去读写一个类的属性，不管这个类是否私有（直接从内存读写）
+     */
 
+    // 从对象的指定偏移地址处读取一个int类型的数据，需要指定要读取的对象和偏移量
     public native int getInt(Object o, long offset);
-
+    //在对象的指定地址出写入一个int类型的数据，需要执行要读取的对象、偏移量和新写入的值。其他类型的数据也有相同的方法，如Object和Boolean
     public native void putInt(Object o, long offset, int x);
-
     public native Object getObject(Object o, long offset);
-
     public native void putObject(Object o, long offset, Object x);
-
     public native boolean getBoolean(Object o, long offset);
-
     public native void    putBoolean(Object o, long offset, boolean x);
-
     public native byte    getByte(Object o, long offset);
-
     public native void    putByte(Object o, long offset, byte x);
-
     public native short   getShort(Object o, long offset);
-
     public native void    putShort(Object o, long offset, short x);
-
     public native char    getChar(Object o, long offset);
-
     public native void    putChar(Object o, long offset, char x);
-
     public native long    getLong(Object o, long offset);
-
     public native void    putLong(Object o, long offset, long x);
-
     public native float   getFloat(Object o, long offset);
-
     public native void    putFloat(Object o, long offset, float x);
-
     public native double  getDouble(Object o, long offset);
-
     public native void    putDouble(Object o, long offset, double x);
 
     @Deprecated
-    public int getInt(Object o, int offset) {
-        return getInt(o, (long)offset);
-    }
-
+    public int getInt(Object o, int offset) {return getInt(o, (long)offset);}
     @Deprecated
-    public void putInt(Object o, int offset, int x) {
-        putInt(o, (long)offset, x);
-    }
-
+    public void putInt(Object o, int offset, int x) {putInt(o, (long)offset, x);}
     @Deprecated
-    public Object getObject(Object o, int offset) {
-        return getObject(o, (long)offset);
-    }
-
+    public Object getObject(Object o, int offset) {return getObject(o, (long)offset);}
     @Deprecated
-    public void putObject(Object o, int offset, Object x) {
-        putObject(o, (long)offset, x);
-    }
-
+    public void putObject(Object o, int offset, Object x) {putObject(o, (long)offset, x);}
     @Deprecated
-    public boolean getBoolean(Object o, int offset) {
-        return getBoolean(o, (long)offset);
-    }
-
+    public boolean getBoolean(Object o, int offset) {return getBoolean(o, (long)offset);}
     @Deprecated
-    public void putBoolean(Object o, int offset, boolean x) {
-        putBoolean(o, (long)offset, x);
-    }
-
+    public void putBoolean(Object o, int offset, boolean x) {putBoolean(o, (long)offset, x);}
     @Deprecated
-    public byte getByte(Object o, int offset) {
-        return getByte(o, (long)offset);
-    }
-
+    public byte getByte(Object o, int offset) {return getByte(o, (long)offset);}
     @Deprecated
-    public void putByte(Object o, int offset, byte x) {
-        putByte(o, (long)offset, x);
-    }
-
+    public void putByte(Object o, int offset, byte x) {putByte(o, (long)offset, x);}
     @Deprecated
-    public short getShort(Object o, int offset) {
-        return getShort(o, (long)offset);
-    }
-
+    public short getShort(Object o, int offset) {return getShort(o, (long)offset);}
     @Deprecated
-    public void putShort(Object o, int offset, short x) {
-        putShort(o, (long)offset, x);
-    }
-
+    public void putShort(Object o, int offset, short x) {putShort(o, (long)offset, x);}
     @Deprecated
-    public char getChar(Object o, int offset) {
-        return getChar(o, (long)offset);
-    }
-
+    public char getChar(Object o, int offset) {return getChar(o, (long)offset);}
     @Deprecated
-    public void putChar(Object o, int offset, char x) {
-        putChar(o, (long)offset, x);
-    }
-
+    public void putChar(Object o, int offset, char x) {putChar(o, (long)offset, x);}
     @Deprecated
-    public long getLong(Object o, int offset) {
-        return getLong(o, (long)offset);
-    }
-
+    public long getLong(Object o, int offset) {return getLong(o, (long)offset);}
     @Deprecated
-    public void putLong(Object o, int offset, long x) {
-        putLong(o, (long)offset, x);
-    }
-
+    public void putLong(Object o, int offset, long x) {putLong(o, (long)offset, x);}
     @Deprecated
-    public float getFloat(Object o, int offset) {
-        return getFloat(o, (long)offset);
-    }
-
+    public float getFloat(Object o, int offset) {return getFloat(o, (long)offset);}
     @Deprecated
-    public void putFloat(Object o, int offset, float x) {
-        putFloat(o, (long)offset, x);
-    }
-
+    public void putFloat(Object o, int offset, float x) {putFloat(o, (long)offset, x);}
     @Deprecated
-    public double getDouble(Object o, int offset) {
-        return getDouble(o, (long)offset);
-    }
-
+    public double getDouble(Object o, int offset) {return getDouble(o, (long)offset);}
     @Deprecated
-    public void putDouble(Object o, int offset, double x) {
-        putDouble(o, (long)offset, x);
-    }
+    public void putDouble(Object o, int offset, double x) {putDouble(o, (long)offset, x);}
 
     // These work on values in the C heap.
-
     public native byte    getByte(long address);
-
     public native void    putByte(long address, byte x);
-
     public native short   getShort(long address);
-
     public native void    putShort(long address, short x);
-
     public native char    getChar(long address);
-
     public native void    putChar(long address, char x);
-
     public native int     getInt(long address);
-
     public native void    putInt(long address, int x);
-
     public native long    getLong(long address);
-
     public native void    putLong(long address, long x);
-
     public native float   getFloat(long address);
-
     public native void    putFloat(long address, float x);
-
     public native double  getDouble(long address);
-
     public native void    putDouble(long address, double x);
-
     public native long getAddress(long address);
-
     public native void putAddress(long address, long x);
+
+
 
     /// wrappers for malloc, realloc, free:
 
+    /**
+     * 直接内存操作
+     */
+    // 分配内存
     public native long allocateMemory(long bytes);
-
+    // 重新设置内存
     public native long reallocateMemory(long address, long bytes);
-
+    // 设置内存
     public native void setMemory(Object o, long offset, long bytes, byte value);
-
     public void setMemory(long address, long bytes, byte value) {
         setMemory(null, address, bytes, value);
     }
-
-    public native void copyMemory(Object srcBase, long srcOffset,
-                                  Object destBase, long destOffset,
-                                  long bytes);
-
+    // 复制
+    public native void copyMemory(Object srcBase, long srcOffset, Object destBase, long destOffset, long bytes);
     public void copyMemory(long srcAddress, long destAddress, long bytes) {
         copyMemory(null, srcAddress, null, destAddress, bytes);
     }
-
+    // 释放内存
     public native void freeMemory(long address);
 
     /// random queries
@@ -235,55 +165,42 @@ public final class Unsafe {
         return null;
     }
 
+    /**
+     * 获取偏移量
+     * 可以通过偏移量和对象得到对象中属性在内存中所处的位置
+     */
+    // 获取静态属性Field在对象中的偏移量
     public native long staticFieldOffset(Field f);
-
+    // 获取非静态属性在对象中的偏移量
     public native long objectFieldOffset(Field f);
-
+    // 返回Field所在对象
     public native Object staticFieldBase(Field f);
-
-    public native boolean shouldBeInitialized(Class<?> c);
-
-    public native void ensureClassInitialized(Class<?> c);
-
+    // 返回数组中第一个元素的偏移量
     public native int arrayBaseOffset(Class<?> arrayClass);
-
-    public static final int ARRAY_BOOLEAN_BASE_OFFSET = theUnsafe.arrayBaseOffset(boolean[].class);
-
-    public static final int ARRAY_BYTE_BASE_OFFSET = theUnsafe.arrayBaseOffset(byte[].class);
-
-    public static final int ARRAY_SHORT_BASE_OFFSET = theUnsafe.arrayBaseOffset(short[].class);
-
-    public static final int ARRAY_CHAR_BASE_OFFSET = theUnsafe.arrayBaseOffset(char[].class);
-
-    public static final int ARRAY_INT_BASE_OFFSET = theUnsafe.arrayBaseOffset(int[].class);
-
-    public static final int ARRAY_LONG_BASE_OFFSET = theUnsafe.arrayBaseOffset(long[].class);
-
-    public static final int ARRAY_FLOAT_BASE_OFFSET = theUnsafe.arrayBaseOffset(float[].class);
-
-    public static final int ARRAY_DOUBLE_BASE_OFFSET = theUnsafe.arrayBaseOffset(double[].class);
-
-    public static final int ARRAY_OBJECT_BASE_OFFSET = theUnsafe.arrayBaseOffset(Object[].class);
-
+    // 返回数组中第一个元素所占用的内存空间
     public native int arrayIndexScale(Class<?> arrayClass);
 
+    public static final int ARRAY_BOOLEAN_BASE_OFFSET = theUnsafe.arrayBaseOffset(boolean[].class);
+    public static final int ARRAY_BYTE_BASE_OFFSET = theUnsafe.arrayBaseOffset(byte[].class);
+    public static final int ARRAY_SHORT_BASE_OFFSET = theUnsafe.arrayBaseOffset(short[].class);
+    public static final int ARRAY_CHAR_BASE_OFFSET = theUnsafe.arrayBaseOffset(char[].class);
+    public static final int ARRAY_INT_BASE_OFFSET = theUnsafe.arrayBaseOffset(int[].class);
+    public static final int ARRAY_LONG_BASE_OFFSET = theUnsafe.arrayBaseOffset(long[].class);
+    public static final int ARRAY_FLOAT_BASE_OFFSET = theUnsafe.arrayBaseOffset(float[].class);
+    public static final int ARRAY_DOUBLE_BASE_OFFSET = theUnsafe.arrayBaseOffset(double[].class);
+    public static final int ARRAY_OBJECT_BASE_OFFSET = theUnsafe.arrayBaseOffset(Object[].class);
     public static final int ARRAY_BOOLEAN_INDEX_SCALE = theUnsafe.arrayIndexScale(boolean[].class);
-
     public static final int ARRAY_BYTE_INDEX_SCALE = theUnsafe.arrayIndexScale(byte[].class);
-
     public static final int ARRAY_SHORT_INDEX_SCALE = theUnsafe.arrayIndexScale(short[].class);
-
     public static final int ARRAY_CHAR_INDEX_SCALE = theUnsafe.arrayIndexScale(char[].class);
-
     public static final int ARRAY_INT_INDEX_SCALE = theUnsafe.arrayIndexScale(int[].class);
-
     public static final int ARRAY_LONG_INDEX_SCALE = theUnsafe.arrayIndexScale(long[].class);
-
     public static final int ARRAY_FLOAT_INDEX_SCALE = theUnsafe.arrayIndexScale(float[].class);
-
     public static final int ARRAY_DOUBLE_INDEX_SCALE = theUnsafe.arrayIndexScale(double[].class);
-
     public static final int ARRAY_OBJECT_INDEX_SCALE = theUnsafe.arrayIndexScale(Object[].class);
+
+
+
 
     public native int addressSize();
 
@@ -294,13 +211,18 @@ public final class Unsafe {
 
     /// random trusted operations from JNI:
 
-    public native Class<?> defineClass(String name, byte[] b, int off, int len,
-                                       ClassLoader loader,
-                                       ProtectionDomain protectionDomain);
-
+    /**
+     * 类加载
+     */
+    // 判断是否需要初始化一个类
+    public native boolean shouldBeInitialized(Class<?> c);
+    // 保证一个类已经被初始化
+    public native void ensureClassInitialized(Class<?> c);
+    // 定义一个类，用于动态创建类
+    public native Class<?> defineClass(String name, byte[] b, int off, int len, ClassLoader loader, ProtectionDomain protectionDomain);
+    // 用于动态创建一个匿名内部类
     public native Class<?> defineAnonymousClass(Class<?> hostClass, byte[] data, Object[] cpPatches);
-
-
+    // 用于创建一个类的实例，但是不会调用这个实例的构造方法，如果这个类还未被初始化，则初始化这个类
     public native Object allocateInstance(Class<?> cls) throws InstantiationException;
 
     @Deprecated
@@ -315,63 +237,56 @@ public final class Unsafe {
     public native void throwException(Throwable ee);
 
 
-    public final native boolean compareAndSwapObject(Object o, long offset,
-                                                     Object expected,
-                                                     Object x);
+    /**
+     * CAS操作
+     * Unsafe中提供了三种类型Object、int、long的CAS操作
+     */
+    public final native boolean compareAndSwapObject(Object o, long offset, Object expected, Object x);
+    public final native boolean compareAndSwapInt(Object o, long offset, int expected, int x);
+    public final native boolean compareAndSwapLong(Object o, long offset, long expected, long x);
 
-    public final native boolean compareAndSwapInt(Object o, long offset,
-                                                  int expected,
-                                                  int x);
 
-    public final native boolean compareAndSwapLong(Object o, long offset,
-                                                   long expected,
-                                                   long x);
-
+    /**
+     * Volatile读写
+     * 可保证有序性和可见性
+     */
     public native Object getObjectVolatile(Object o, long offset);
-
     public native void    putObjectVolatile(Object o, long offset, Object x);
-
     public native int     getIntVolatile(Object o, long offset);
-
     public native void    putIntVolatile(Object o, long offset, int x);
-
     public native boolean getBooleanVolatile(Object o, long offset);
-
     public native void    putBooleanVolatile(Object o, long offset, boolean x);
-
     public native byte    getByteVolatile(Object o, long offset);
-
     public native void    putByteVolatile(Object o, long offset, byte x);
-
     public native short   getShortVolatile(Object o, long offset);
-
     public native void    putShortVolatile(Object o, long offset, short x);
-
     public native char    getCharVolatile(Object o, long offset);
-
     public native void    putCharVolatile(Object o, long offset, char x);
-
     public native long    getLongVolatile(Object o, long offset);
-
     public native void    putLongVolatile(Object o, long offset, long x);
-
     public native float   getFloatVolatile(Object o, long offset);
-
     public native void    putFloatVolatile(Object o, long offset, float x);
-
     public native double  getDoubleVolatile(Object o, long offset);
-
     public native void    putDoubleVolatile(Object o, long offset, double x);
 
+    /**
+     * 有序写入
+     * 有序写入只保证写入的有序性，并不保证可见性，也就是说，该线程的写入不保证其他线程能立马看到
+     */
     public native void    putOrderedObject(Object o, long offset, Object x);
-
     public native void    putOrderedInt(Object o, long offset, int x);
-
     public native void    putOrderedLong(Object o, long offset, long x);
 
-    public native void unpark(Object thread);
 
+    /**
+     * 线程调度
+     * LockSupport中的park()和unpark()方法就是依靠Unsafe中的这两个方法实现的
+     * 线程唤醒和阻塞
+     */
+    public native void unpark(Object thread);
     public native void park(boolean isAbsolute, long time);
+
+
 
     public native int getLoadAverage(double[] loadavg, int nelems);
 
@@ -419,10 +334,14 @@ public final class Unsafe {
     }
 
 
+    /**
+     * 内存屏障
+     */
+    // 保证这个屏障之前所有读操作已经完成
     public native void loadFence();
-
+    // 保证这个屏障之前所有写操作已经完成
     public native void storeFence();
-
+    // 保证这个屏障之前所有读写操作都完成
     public native void fullFence();
 
     private static void throwIllegalAccessError() {
