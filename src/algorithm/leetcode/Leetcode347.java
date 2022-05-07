fed96d61123b14e4c64cef58ce759f4f21128f7f
caoyang42 <caoyang42@meituan.com>
2022-05-07 12:29:59 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode347.java
+++ src/algorithm/leetcode/Leetcode347.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.PriorityQueue;
 
 /**
- * 347. 前 K 个高频元素
+ * 347. 前K个高频元素
  * 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按任意顺序返回答案
  *
  * 输入: nums = [1,1,1,2,2,3], k = 2
