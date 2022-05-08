b68c7103c37eed61d38d0a0360d801668ef90d2e
caoyang42 <caoyang42@meituan.com>
2022-05-08 12:37:02 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode349.java
+++ src/algorithm/leetcode/Leetcode349.java
@@ -1,7 +1,50 @@
 package src.algorithm.leetcode;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
+ * 349. 两个数组的交集
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[9,4]
  * @author caoyang
  */
 public class Leetcode349 {
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for (int num : nums1) {
+            set.add(num);
+            set1.add(num);
+        }
+        for (int num : nums2) {
+            set.add(num);
+            set2.add(num);
+        }
+        for (int num : set1) {
+            if(!set2.contains(num)){
+                set.remove(num);
+            }
+        }
+        for (int num : set2) {
+            if(!set1.contains(num)){
+                set.remove(num);
+            }
+        }
+        int[] ans = new int[set.size()];
+        int index = 0;
+        for (int num : set) {
+            ans[index++] = num;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {4,9,5};
+        int[] nums2 = {9,4,9,8,4};
+        int[] result = intersection(nums1, nums2);
+        System.out.println(Arrays.toString(result));
+    }
 }
--- src/algorithm/leetcode/Leetcode378.java
+++ src/algorithm/leetcode/Leetcode378.java
@@ -1,7 +1,34 @@
 package src.algorithm.leetcode;
 
+import java.util.Arrays;
+
 /**
+ * 378. 有序矩阵中第K小的元素
+ * 它是排序后的第 k 小元素，而不是第k个不同的元素
+ *
+ * 输入：matrix = [[1,5,9],[10,11,13],[12,13,15]], k = 8
+ * 输出：13
  * @author caoyang
  */
 public class Leetcode378 {
+    public static int kthSmallest(int[][] matrix, int k) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] nums = new int[m * n];
+        int index = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                nums[index++] = matrix[i][j];
+            }
+        }
+        Arrays.sort(nums);
+        return nums[k-1];
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2},{1,3}};
+        int k = 4;
+        int result = kthSmallest(matrix, k);
+        System.out.println(result);
+    }
 }
