7bebfcde0b4ddec1b537182f67378cd9bdfce1cd
caoyang42 <caoyang42@meituan.com>
2022-05-09 23:59:10 +0800

    欢迎入坑Java，开启从入门到放弃之路

---
+++ src/basis/fish/IntegerTest.java
@@ -0,0 +1,88 @@
+package src.basis.fish;
+
+import org.junit.Test;
+
+/**
+ * Integer 测试
+ * @author caoyang
+ */
+public class IntegerTest {
+
+    @Test
+    public void integerTest1(){
+        Integer a = 127;
+        Integer b = 127;
+        Integer c = new Integer(127);
+        int d =127;
+        // true
+        System.out.println("127 a==b: " + (a==b));
+        // false
+        System.out.println("127 b==c: " + (b==c));
+        // true
+        System.out.println("127 b==d: " + (b==d));
+        // true
+        System.out.println("127 c==d: " + (c==d));
+        // true
+        System.out.println("127 a==d: " + (a==d));
+
+        Integer e = 128;
+        Integer f = 128;
+        int g = 128;
+        // false
+        System.out.println("128 e==f: " + (e==f));
+        // true
+        System.out.println("128 e==g: " + (e==g));
+    }
+
+    @Test
+    public void integerTest2(){
+        Integer a = 1;
+        Integer b = 2;
+        Integer c = 3;
+        Integer d = 3;
+        Integer e = 321;
+        Integer f = 321;
+        Long g = 3L;
+        // true
+        System.out.println("c==d: " + (c==d));
+        // false
+        System.out.println("e==f: " + (e==f));
+        // true
+        System.out.println(c == (a+b));
+        // true 类型和数值一样
+        System.out.println(c.equals(a+b));
+        // true 比较数值
+        System.out.println(g == (a+b));
+        // false 类型不一样
+        System.out.println(g.equals(a+b));
+    }
+
+    @Test
+    public void integerTest3(){
+        int a=10;
+        // m=7+a   a=a+1
+        int m=7+a++;
+        //11
+        System.out.println(a);
+        //17
+        System.out.println(m);
+    }
+
+    @Test
+    public void integerTest4(){
+        /**
+         *     iconst_0
+         *     istore 0     0赋值给i
+         *     iload 0      压栈
+         *     iinc 0,1     i自增
+         *     istore 0     弹栈 0
+         *     getstatic 'java/lang/System.out','Ljava/io/PrintStream;'
+         *     iload 0      0 赋值给i，自增白忙活
+         */
+        int i = 0;
+        // i++ 更改的值不会被使用
+        i = i++;
+        System.out.println("interest: "+i);
+    }
+
+}
--- src/basis/fish/StringTest1.java
+++ src/basis/fish/StringTest1.java
@@ -1,10 +1,13 @@
 package src.basis.fish;
 
+import org.junit.Test;
+
 /**
  * @author caoyang
  */
 public class StringTest1 {
-    public static void main(String[] args) {
+    @Test
+    public void stringTest1() {
         String a = new String("xyz");
         String b = new String("xyz");
         // false
@@ -21,7 +24,10 @@ public class StringTest1 {
         // true
         System.out.println(c1 == d);
 
+    }
 
+    @Test
+    public void stringTest2(){
         String x = "java";
         String y = new StringBuilder().append("ja").append("va").toString();
         // 由于"java"在常量池已出现过，y.intern 返回 x 的地址，与 y 不同
@@ -31,17 +37,28 @@ public class StringTest1 {
         String z = new StringBuilder().append("wor").append("ld").toString();
         // true
         System.out.println(z.intern() == z);
+    }
 
+    @Test
+    public void stringTest3() {
+        final String a = "java";
+        final String b = "world";
+        String c = "javaworld";
+        String d = a + b;
+        // true
+        System.out.println(c == d);
+    }
+
+    @Test
+    public void stringTest4(){
         /*
         字符串长度是由UTF-16编码单元个数决定
         char s = '𝄞'; 编译检查会提示字符过多，𝄞 实际占两个字符
          */
-        String B = "𝄞";
+        String a = "𝄞";
         // 音符字符的UTF-16编码
-        String C = "\uD834\uDD1E";
-        System.out.println(C);
-        System.out.println(B.length());
-
-
+        String b = "\uD834\uDD1E";
+        System.out.println(b);
+        System.out.println(a.length());
     }
 }
