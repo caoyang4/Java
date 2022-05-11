7d8d33d2b8174ae77241c23adc46503e0c4a6f52
caoyang42 <caoyang42@meituan.com>
2022-05-11 12:56:49 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode395.java
+++ src/algorithm/leetcode/Leetcode395.java
@@ -1,7 +1,9 @@
 package src.algorithm.leetcode;
 
+import java.util.*;
+
 /**
- * 395. 至少有 K 个重复字符的最长子串
+ * 395. 至少有K个重复字符的最长子串
  * 给你一个字符串 s 和一个整数 k ，请你找出 s 中的最长子串， 要求该子串中的每一字符出现次数都不少于 k
  * 输入：s = "ababbc", k = 2
  * 输出：5
@@ -9,4 +11,31 @@ package src.algorithm.leetcode;
  * @author caoyang
  */
 public class Leetcode395 {
+    // 对于字符串 ss，如果存在某个字符ch，它的出现次数大于 00 且小于 kk，则任何包含ch的子串都不可能满足要求。
+    // 也就是说，我们将字符串按照ch切分成若干段，则满足要求的最长子串一定出现在某个被切分的段内，而不能跨越一个或多个段
+    public static int longestSubstring(String s, int k) {
+        if (s.length() < k){return 0;}
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
+        }
+        for (char c : map.keySet()) {
+            if (map.get(c) < k){
+                int max = 0;
+                for (String t : s.split(String.valueOf(c))) {
+                    max = Math.max(max, longestSubstring(t, k));
+                }
+                return max;
+            }
+        }
+        return s.length();
+    }
+
+    public static void main(String[] args) {
+        String s = "dababbc";
+        int k = 2;
+        int result = longestSubstring(s, k);
+        System.out.println(result);
+    }
+
 }
--- src/algorithm/leetcode/Leetcode400.java
+++ src/algorithm/leetcode/Leetcode400.java
@@ -1,7 +1,44 @@
 package src.algorithm.leetcode;
 
 /**
+ * 400. 第 N 位数字
+ *
+ * 给你一个整数 n ，请你在无限的整数序列 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...] 中找出并返回第 n 位上的数字
+ *
+ * 输入：n = 11
+ * 输出：0
+ * 解释：第 11 位数字在序列 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... 里是 0 ，它是 10 的一部分
  * @author caoyang
  */
 public class Leetcode400 {
+    public static int findNthDigit(int n) {
+        long start = 1;
+        long multiple = 1;
+        long remain = n;
+        while ((remain = remain - 9 * multiple * start) > 0){
+            start++;
+            multiple *= 10;
+        }
+
+        if (remain == 0){
+            return 9;
+        } else{
+            remain +=  9 * multiple * start;
+            long nthNum = remain / start;
+            long nthDigit = remain % start;
+            if (nthDigit == 0){
+                String num = String.valueOf(multiple+nthNum-1);
+                return Integer.parseInt(num.substring(num.length()-1));
+            } else {
+                String num = String.valueOf(multiple+nthNum);
+                return Integer.parseInt(num.substring((int)nthDigit-1, (int)nthDigit));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 2147483647;
+        int result = findNthDigit(n);
+        System.out.println(result);
+    }
 }
