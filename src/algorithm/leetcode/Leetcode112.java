08ab8d6b39ff172db1c411155938a34f835dc81e
caoyang42 <caoyang42@meituan.com>
2022-05-16 10:40:14 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode112.java
+++ src/algorithm/leetcode/Leetcode112.java
@@ -2,7 +2,7 @@ package src.algorithm.leetcode;
 
 /**
  * 112. 路径总和
- * 给你二叉树的根节点root 和一个表示目标和的整数targetSum 。
+ * 给你二叉树的根节点root和一个表示目标和的整数targetSum 。
  * 判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和targetSum 。
  * 如果存在，返回 true ；否则，返回 false
  *
--- src/algorithm/leetcode/Leetcode437.java
+++ src/algorithm/leetcode/Leetcode437.java
@@ -1,7 +1,43 @@
 package src.algorithm.leetcode;
 
+
 /**
+ * 437. 路径总和 III
+ * 输入：root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+ * 输出：3
+ * 解释：和等于 8 的路径有 3 条
+ *
  * @author caoyang
  */
 public class Leetcode437 {
+    // 访问每一个节点node，检测以node为起始节点且向下延深的路径有多少种
+    public int pathSum(TreeNode root, int targetSum) {
+        int num = 0;
+        if (root == null){return num;}
+        // 根节点为起始节点搜索
+        num += trackBack(root, targetSum);
+        // 左节点为起始节点搜索
+        num += pathSum(root.left, targetSum);
+        // 右节点为起始节点搜索
+        num += pathSum(root.right, targetSum);
+        return num;
+    }
+
+    public int trackBack(TreeNode root, int targetSum){
+        int num = 0;
+        if (root == null){
+            return 0;
+        }
+        if (targetSum == root.val){
+            num++;
+        }
+        num += trackBack(root.left, targetSum-root.val);
+        num += trackBack(root.right, targetSum-root.val);
+        return num;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
