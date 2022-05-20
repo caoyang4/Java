5835dd836a2ac41977b6bdb837642a0dae448531
caoyang42 <caoyang42@meituan.com>
2022-05-20 12:43:00 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode445.java
+++ src/algorithm/leetcode/Leetcode445.java
@@ -1,7 +1,53 @@
 package src.algorithm.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * 445. 两数相加 II
+ * 给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置
+ * 输入：l1 = [7,2,4,3], l2 = [5,6,4]
+ * 输出：[7,8,0,7]
  * @author caoyang
  */
 public class Leetcode445 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        while (l1 != null || l2 != null){
+            if(l1 != null){
+                list1.add(l1.val);
+                l1 = l1.next;
+            }
+            if(l2 != null){
+                list2.add(l2.val);
+                l2 = l2.next;
+            }
+        }
+        int end1 = list1.size()-1;
+        int end2 = list2.size()-1;
+        List<Integer> result = new ArrayList<>();
+        int carry = 0;
+        while (end1 >= 0 || end2 >= 0){
+            int val1 = end1 >= 0 ? list1.get(end1--) : 0;
+            int val2 = end2 >= 0 ? list2.get(end2--) : 0;
+            int sum = val1 + val2 + carry;
+            int remain = sum % 10;
+            carry = sum / 10;
+            result.add(remain);
+        }
+        if (carry > 0){
+            result.add(carry);
+        }
+        ListNode root = new ListNode();
+        ListNode cursor = root;
+        for (int i = result.size()-1; i  >= 0; i--) {
+            cursor = cursor.next = new ListNode(result.get(i));
+        }
+        return root.next;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/algorithm/leetcode/Leetcode450.java
+++ src/algorithm/leetcode/Leetcode450.java
@@ -1,7 +1,49 @@
 package src.algorithm.leetcode;
 
 /**
+ * 450. 删除二叉搜索树中的节点
+ * 给定一个二叉搜索树的根节点 root 和一个值 key，删除二叉搜索树中的key对应的节点，并保证二叉搜索树的性质不变。
+ * 返回二叉搜索树（有可能被更新）的根节点的引用
+ *
  * @author caoyang
  */
 public class Leetcode450 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        // 右子树
+        if (key > root.val) root.right = deleteNode(root.right, key);
+        // 左子树
+        else if (key < root.val) root.left = deleteNode(root.left, key);
+        else {
+            // 叶子结点
+            if (root.left == null && root.right == null) root = null;
+            // 有右节点
+            else if (root.right != null) {
+                root.val = getSuccessor(root);
+                // 删除后继节点
+                root.right = deleteNode(root.right, root.val);
+            }
+            // 只有左节点
+            else {
+                root.val = getPredecessor(root);
+                // 删除前驱节点
+                root.left = deleteNode(root.left, root.val);
+            }
+        }
+        return root;
+    }
+    public int getPredecessor(TreeNode root){
+        root = root.left;
+        while (root.right != null) root = root.right;
+        return root.val;
+    }
+    public int getSuccessor(TreeNode root){
+        root = root.right;
+        while (root.left != null) root = root.left;
+        return root.val;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
