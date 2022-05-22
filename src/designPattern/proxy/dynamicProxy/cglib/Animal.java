9248db975cce0696ab0e5f290c23121db57f5dd2
caoyang42 <caoyang42@meituan.com>
2022-05-22 22:45:00 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/designPattern/proxy/dynamicProxy/cglib/Animal.java
+++ src/designPattern/proxy/dynamicProxy/cglib/Animal.java
@@ -22,13 +22,21 @@ public class Animal {
         Enhancer enhancer = new Enhancer();
         enhancer.setClassLoader(Animal.class.getClassLoader());
         enhancer.setSuperclass(Animal.class);
+        // MethodInterceptor 拦截器
+        // intercept方法的参数依次为：代理类、拦截的方法、参数、方法代理
         enhancer.setCallback((MethodInterceptor) (o, method, objects, methodProxy) -> {
             System.out.println("cglib start to " + method.getName());
             Object obj = method.invoke(animal, objects);
             System.out.println("cglib end to " + method.getName());
             return obj;
         });
+        /*
+            调用Enhancer.create()时大致流程如下：
+                调用createHelper()方法然后进行前置条件检查；
+                调用Enhancer内部接口EnhancerKey的newInstance(···)生成唯一key（用于从缓存中获取代理类实例）。
+                然后通过super.create(key)获取我们设置的SuperClass对应的代理类实例。
 
+         */
         Animal animalCglib = (Animal) enhancer.create();
         animalCglib.eat();
         System.out.println("=============");
