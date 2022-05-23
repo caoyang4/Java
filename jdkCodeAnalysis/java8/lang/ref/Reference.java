a1db28e27d80258c7e13224e4cc5529d0cc9a090
caoyang42 <caoyang42@meituan.com>
2022-05-23 16:43:53 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- jdkCodeAnalysis/java8/lang/ref/Reference.java
+++ jdkCodeAnalysis/java8/lang/ref/Reference.java
@@ -1,43 +1,8 @@
-/*
- * Copyright (c) 1997, 2017, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
 package java.lang.ref;
 
 import sun.misc.Cleaner;
 import sun.misc.JavaLangRefAccess;
 import sun.misc.SharedSecrets;
-
-/**
- * Abstract base class for reference objects.  This class defines the
- * operations common to all reference objects.  Because reference objects are
- * implemented in close cooperation with the garbage collector, this class may
- * not be subclassed directly.
- *
- * @author   Mark Reinhold
- * @since    1.2
- */
 
 public abstract class Reference<T> {
 
@@ -155,22 +120,6 @@ public abstract class Reference<T> {
         }
     }
 
-    /**
-     * Try handle pending {@link Reference} if there is one.<p>
-     * Return {@code true} as a hint that there might be another
-     * {@link Reference} pending or {@code false} when there are no more pending
-     * {@link Reference}s at the moment and the program can do some other
-     * useful work instead of looping.
-     *
-     * @param waitForNotify if {@code true} and there was no pending
-     *                      {@link Reference}, wait until notified from VM
-     *                      or interrupted; if {@code false}, return immediately
-     *                      when there is no pending {@link Reference}.
-     * @return {@code true} if there was a {@link Reference} pending and it
-     *         was processed, or we waited for notification and either got it
-     *         or thread was interrupted before being notified;
-     *         {@code false} otherwise.
-     */
     static boolean tryHandlePending(boolean waitForNotify) {
         Reference<Object> r;
         Cleaner c;
@@ -242,25 +191,10 @@ public abstract class Reference<T> {
 
     /* -- Referent accessor and setters -- */
 
-    /**
-     * Returns this reference object's referent.  If this reference object has
-     * been cleared, either by the program or by the garbage collector, then
-     * this method returns <code>null</code>.
-     *
-     * @return   The object to which this reference refers, or
-     *           <code>null</code> if this reference object has been cleared
-     */
     public T get() {
         return this.referent;
     }
 
-    /**
-     * Clears this reference object.  Invoking this method will not cause this
-     * object to be enqueued.
-     *
-     * <p> This method is invoked only by Java code; when the garbage collector
-     * clears references it does so directly, without invoking this method.
-     */
     public void clear() {
         this.referent = null;
     }
@@ -268,30 +202,10 @@ public abstract class Reference<T> {
 
     /* -- Queue operations -- */
 
-    /**
-     * Tells whether or not this reference object has been enqueued, either by
-     * the program or by the garbage collector.  If this reference object was
-     * not registered with a queue when it was created, then this method will
-     * always return <code>false</code>.
-     *
-     * @return   <code>true</code> if and only if this reference object has
-     *           been enqueued
-     */
     public boolean isEnqueued() {
         return (this.queue == ReferenceQueue.ENQUEUED);
     }
 
-    /**
-     * Adds this reference object to the queue with which it is registered,
-     * if any.
-     *
-     * <p> This method is invoked only by Java code; when the garbage collector
-     * enqueues references it does so directly, without invoking this method.
-     *
-     * @return   <code>true</code> if this reference object was successfully
-     *           enqueued; <code>false</code> if it was already enqueued or if
-     *           it was not registered with a queue when it was created
-     */
     public boolean enqueue() {
         return this.queue.enqueue(this);
     }
