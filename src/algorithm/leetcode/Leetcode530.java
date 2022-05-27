b969f87bb289f97bb76edc44c5fcdf702a13cbe6
caoyang42 <caoyang42@meituan.com>
2022-05-27 14:29:16 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode530.java
+++ src/algorithm/leetcode/Leetcode530.java
@@ -1,7 +1,31 @@
 package src.algorithm.leetcode;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
+ * 530. 二叉搜索树的最小绝对差
  * @author caoyang
  */
 public class Leetcode530 {
+    public int getMinimumDifference(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        traverse(list, root);
+        int min = Integer.MAX_VALUE;
+        for (int i = 1; i < list.size(); i++) {
+            min = Math.min(min, list.get(i)-list.get(i-1));
+        }
+        return min;
+    }
+    public void traverse(List<Integer> list, TreeNode root){
+        if (root == null) return;;
+        traverse(list, root.left);
+        list.add(root.val);
+        traverse(list, root.right);
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/algorithm/leetcode/Leetcode543.java
+++ src/algorithm/leetcode/Leetcode543.java
@@ -1,7 +1,25 @@
 package src.algorithm.leetcode;
 
 /**
+ * 543. 二叉树的直径
  * @author caoyang
  */
 public class Leetcode543 {
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+        return trackBack(root)[0];
+    }
+    public int[] trackBack(TreeNode root){
+        if (root == null || (root.left == null && root.right == null)){
+            return new int[]{0, 0};
+        }
+        int[] left = trackBack(root.left);
+        int[] right = trackBack(root.right);
+        int r = left[1] + right[1] + (root.left != null && root.right != null ? 2 : 1);
+        return new int[]{Math.max(Math.max(left[0], right[0]), r), Math.max(left[1], right[1])+1};
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
