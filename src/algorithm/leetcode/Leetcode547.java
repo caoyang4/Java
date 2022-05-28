ae479233b1d89774aaa02cf1a903a1cbefaa1d5c
caoyang42 <caoyang42@meituan.com>
2022-05-28 12:45:53 +0800

    欢迎入坑Java，开启从入门到放弃之路

--- src/algorithm/leetcode/Leetcode547.java
+++ src/algorithm/leetcode/Leetcode547.java
@@ -1,7 +1,36 @@
 package src.algorithm.leetcode;
 
 /**
+ * 547. 省份数量
+ * 输入：isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+ * 输出：2
  * @author caoyang
  */
 public class Leetcode547 {
+    public static int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] used = new boolean[n];
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!used[i]){
+                trackBack(isConnected, used, n, i);
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void trackBack(int[][] isConnected, boolean[] used, int n, int row){
+        for (int j = 0; j < n; j++) {
+            if (!used[j] && isConnected[row][j] == 1){
+                used[j] = true;
+                trackBack(isConnected, used, n, j);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] isConnected = {{1,1,0}, {1,1,0}, {0,0,1}};
+        int result = findCircleNum(isConnected);
+        System.out.println(result);
+    }
 }
