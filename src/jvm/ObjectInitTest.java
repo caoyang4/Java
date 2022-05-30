1d3852ae61478275bdf125bab5f071cba9a94bd2
caoyang42 <caoyang42@meituan.com>
2022-05-30 23:34:19 +0800

    锁优化

--- src/jvm/ObjectInitTest.java
+++ src/jvm/ObjectInitTest.java
@@ -9,11 +9,21 @@ public class ObjectInitTest {
     static class Father{
         public Father() {
             System.out.println("Father init");
+            // 不一定调用父类的 eat() 方法，多态机制
+            eat();
+        }
+        public void eat(){
+            System.out.println("father eat beef");
         }
     }
     static class Child extends Father{
         public Child() {
             System.out.println("Child init");
+        }
+
+        @Override
+        public void eat() {
+            System.out.println("child eat sugar");
         }
     }
 
