ca7a2cef8007f12f822aa2cebf6c757a0200b0da
caoyang42 <caoyang42@meituan.com>
2022-05-30 23:13:22 +0800

    锁优化

--- src/juc/lock/TestBiasDismiss2.java
+++ src/juc/lock/TestBiasDismiss2.java
@@ -5,6 +5,10 @@ import org.openjdk.jol.info.ClassLayout;
 
 /**
  * 其他线程竞争导致偏向锁撤销
+ * 当调用锁对象的Object#hashcode()或System.identityHashCode()方法会导致该对象的偏向锁或轻量级锁升级
+ * 如果是无锁状态则存放在mark word中，如果是重量级锁则存放在对应的monitor中，而偏向锁是没有地方能存放该信息的，所以必须升级
+ *   当对象可偏向时，MarkWord将变成未锁定状态，并只能升级成轻量锁；
+ *   当对象正处于偏向锁时，调用HashCode将使偏向锁强制升级成重量锁。
  * @author caoyang
  */
 @Slf4j(topic = "TestBiasDismiss2")
