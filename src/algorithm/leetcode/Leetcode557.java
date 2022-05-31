8e53297af891413dcb91722ea510e8f77c9fcb1d
caoyang42 <caoyang42@meituan.com>
2022-05-31 17:20:37 +0800

    jvm

--- src/algorithm/leetcode/Leetcode557.java
+++ src/algorithm/leetcode/Leetcode557.java
@@ -1,7 +1,26 @@
 package src.algorithm.leetcode;
 
 /**
+ * 557. 反转字符串中的单词 III
+ *
+ * 输入：s = "Let's take LeetCode contest"
+ * 输出："s'teL ekat edoCteeL tsetnoc"
  * @author caoyang
  */
 public class Leetcode557 {
+    public static String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder builder = new StringBuilder();
+        for (String word : words) {
+            StringBuilder tmp = new StringBuilder(word);
+            builder.append(tmp.reverse()).append(" ");
+        }
+        return builder.substring(0, s.length());
+    }
+
+    public static void main(String[] args) {
+        String s = "Let's take LeetCode contest";
+        String result = reverseWords(s);
+        System.out.println(result);
+    }
 }
--- src/algorithm/leetcode/Leetcode560.java
+++ src/algorithm/leetcode/Leetcode560.java
@@ -1,7 +1,49 @@
 package src.algorithm.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
+ * 560. 和为K的子数组
+ * 输入：nums = [1,1,1], k = 2
+ * 输出：2
  * @author caoyang
  */
 public class Leetcode560 {
+    // 前缀和
+    public static int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        // 处理 sum(0,i) = k 的情况
+        map.put(0, 1);
+        int count = 0;
+        int preSum = 0;
+        for (int num : nums) {
+            preSum += num;
+            count += map.getOrDefault(preSum-k, 0);
+            map.put(preSum, map.getOrDefault(preSum, 0) + 1);
+        }
+        return count;
+    }
+    // dfs 超时
+    public static int subarraySum1(int[] nums, int k) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count += trackBack(nums, i, k, k);
+        }
+        return count;
+    }
+    public static int trackBack(int[] nums, int start, int target, int origin){
+        if (start >= nums.length) return 0;
+        if (target == nums[start]) {
+            return trackBack(nums, start+1, 0, 0) + 1;
+        }
+        return trackBack(nums, start+1, target-nums[start], target);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,-1,0,0,0};
+        int k = 0;
+        int result = subarraySum(nums, k);
+        System.out.println(result);
+    }
 }
