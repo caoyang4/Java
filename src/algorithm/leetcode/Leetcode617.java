9516646872608d711c301bdf06013bf301d69854
caoyang42 <caoyang42@meituan.com>
2022-06-02 11:46:52 +0800

    leetcode

--- src/algorithm/leetcode/Leetcode617.java
+++ src/algorithm/leetcode/Leetcode617.java
@@ -1,7 +1,21 @@
 package src.algorithm.leetcode;
 
 /**
+ * 617. 合并二叉树
  * @author caoyang
  */
 public class Leetcode617 {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return null;
+        int val = root1 == null ? 0 : root1.val;
+        val += root2 == null ? 0 : root2.val;
+        TreeNode result = new TreeNode(val);
+        result.left = mergeTrees(root1 == null ? null : root1.left, root2 == null ? null : root2.left);
+        result.right = mergeTrees(root1 == null ? null : root1.right, root2 == null ? null : root2.right);
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/algorithm/leetcode/Leetcode622.java
+++ src/algorithm/leetcode/Leetcode622.java
@@ -1,7 +1,60 @@
 package src.algorithm.leetcode;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
+ * 622. 设计循环队列
+ * 基于FIFO（先进先出）
  * @author caoyang
  */
 public class Leetcode622 {
+    class MyCircularQueue {
+
+        Deque<Integer> list;
+        int size = 0;
+        int capacity;
+        public MyCircularQueue(int k) {
+            capacity = k;
+            list = new LinkedList<>();
+        }
+        // 入队
+        public boolean enQueue(int value) {
+            if (list.size() >= capacity){
+                return false;
+            }
+            if (size == list.size() || size < capacity){
+                list.add(value);
+                size++;
+            } else {
+                list.addFirst(value);
+            }
+            return true;
+        }
+        // 出队
+        public boolean deQueue() {
+            if(isEmpty()) return false;
+            list.pop();
+            size--;
+            return true;
+        }
+        // 队首获取元素
+        public int Front() {
+            if (isEmpty()) return -1;
+            return list.getFirst();
+        }
+        // 队尾获取元素
+        public int Rear() {
+            if (isEmpty()) return -1;
+            return list.getLast();
+        }
+
+        public boolean isEmpty() {
+            return list.isEmpty();
+        }
+
+        public boolean isFull() {
+            return list.size() == capacity;
+        }
+    }
 }
