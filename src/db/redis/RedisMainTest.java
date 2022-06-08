d93feb38d6e22511ecd5bab4a8a7014fc3e45446
caoyang42 <caoyang42@meituan.com>
2022-06-08 18:28:31 +0800

    jedis

--- src/db/redis/RedisMainTest.java
+++ src/db/redis/RedisMainTest.java
@@ -1,14 +1,67 @@
 package src.db.redis;
 
+import org.junit.Test;
 import redis.clients.jedis.Jedis;
 import org.openjdk.jol.info.ClassLayout;
 
+import java.util.List;
+import java.util.Set;
+
 public class RedisMainTest {
-    public static void main(String[] args) {
-        Jedis jedis = new Jedis("127.0.0.1", 6379);
+    final Jedis jedis = new Jedis("127.0.0.1", 6379);
+
+    @Test
+    public void testConnection(){
         System.out.println(jedis.ping());
-        String name = jedis.get("caoyang");
-        System.out.println(name);
+    }
+
+    @Test
+    public void testExists(){
+        Long exists = jedis.exists("city", "ball", "notExists");
+        System.out.println(exists);
+    }
+
+    @Test
+    public void testKeys(){
+        Set<String> keys = jedis.keys("*");
+        keys.forEach(System.out::println);
+    }
+
+    @Test
+    public void testString(){
+        jedis.set("client", "jedis");
+        jedis.setnx("client", "nx");
+        jedis.setnx("clientnx", "nx");
+        System.out.println(jedis.get("caoyang"));
+        System.out.println(jedis.get("clientnx"));
+        System.out.println(jedis.get("client"));
+    }
+
+    @Test
+    public void testList(){
+        List<String> citys = jedis.lrange("city", 0, -1);
+        citys.forEach(System.out::println);
+        jedis.lpush("city", "paris", "newyork");
+        System.out.println(jedis.lpop("city"));
+    }
+
+    @Test
+    public void testSet(){
+        jedis.sadd("fruits", "apple", "pear", "grape");
+        jedis.smembers("fruits").forEach(System.out::println);
+        jedis.sadd("set1", "a", "b", "c");
+        jedis.sadd("set2", "c", "d", "f");
+        System.out.println(jedis.sdiff("set1", "set2"));
+        System.out.println(jedis.sinter("set1", "set2"));
+        System.out.println(jedis.sunion("set1", "set2"));
+    }
+
+    @Test
+    public void testHash(){
+        jedis.hset("user", "name","wukong");
+        jedis.hset("user", "age","10000");
+        jedis.hset("user", "nation","China");
+        System.out.println(jedis.hmget("user", "name", "age", "nation"));
 
     }
 }
