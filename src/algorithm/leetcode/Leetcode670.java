4ffac727fde1911ada143839888fb7c0a628d1d3
caoyang42 <caoyang42@meituan.com>
2022-06-08 17:01:03 +0800

    leetcode

--- src/algorithm/leetcode/Leetcode670.java
+++ src/algorithm/leetcode/Leetcode670.java
@@ -1,7 +1,38 @@
 package src.algorithm.leetcode;
 
 /**
+ * 670. 最大交换
+ * 给定一个非负整数，你至多可以交换一次数字中的任意两位。返回你能得到的最大值
+ * 输入: 2736
+ * 输出: 7236
  * @author caoyang
  */
 public class Leetcode670 {
+    public static int maximumSwap(int num) {
+        char[] chars = String.valueOf(num).toCharArray();
+        int n = chars.length;
+
+        for (int i = 0; i < n-1; i++) {
+            int t = n - 1;
+            char max = chars[i];
+            for (int j = n-1; j > i; j--) {
+                if (chars[j] > max){
+                    t = j;
+                    max = chars[j];
+                }
+            }
+            if (max > chars[i]){
+                chars[t] = chars[i];
+                chars[i] = max;
+                break;
+            }
+        }
+        return Integer.parseInt(String.valueOf(chars));
+    }
+
+    public static void main(String[] args) {
+        int num = 27367;
+        int result = maximumSwap(num);
+        System.out.println(result);
+    }
 }
--- src/algorithm/leetcode/Leetcode673.java
+++ src/algorithm/leetcode/Leetcode673.java
@@ -1,7 +1,47 @@
 package src.algorithm.leetcode;
 
+
 /**
+ * 673. 最长递增子序列的个数
+ * 给定一个未排序的整数数组nums，返回最长递增子序列的个数，注意数列必须是严格递增的
+ * 输入: [1,3,5,4,7]
+ * 输出: 2
  * @author caoyang
  */
 public class Leetcode673 {
+    public static int findNumberOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        int[] count = new int[n];
+        int len = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = count[i] = 1;
+            for (int j = i-1; j >= 0; j--) {
+                if(nums[j] < nums[i]){
+                    // 子序列继续递增
+                    if (dp[i] < dp[j]+1){
+                        dp[i] = dp[j] + 1;
+                        count[i] = count[j];
+                    // 递增终止
+                    } else if (dp[i] == dp[j] + 1){
+                        count[i] += count[j];
+                    }
+                }
+            }
+            len = Math.max(len, dp[i]);
+        }
+        int res = 0;
+        for (int i = 0; i < dp.length; i++) {
+            if(dp[i] == len){
+                res += count[i];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,4,3,5,4,7};
+        int result = findNumberOfLIS(nums);
+        System.out.println(result);
+    }
 }
