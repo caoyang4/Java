238eab27826a9a57d497a94504ea6187b5f9db44
caoyang42 <caoyang42@meituan.com>
2022-09-09 14:37:16 +0800

    反射

---
+++ src/basis/reflection/BaseTest.java
@@ -0,0 +1,14 @@
+package src.basis.reflection;
+
+import org.junit.Test;
+
+/**
+ * @author caoyang
+ * @create 2022-09-09 14:36
+ */
+public class BaseTest {
+    @Test
+    public void test1(){
+        System.out.println(String.class.isAssignableFrom(String.class));
+    }
+}
