93d21b7efd2e57b4c0f8eb9f3a18aa6add11c07d
caoyang42 <caoyang42@meituan.com>
2023-05-04 16:42:17 +0800

    merge

--- src/basis/fish/LoopTest.java
+++ src/basis/fish/LoopTest.java
@@ -1,5 +1,7 @@
 package src.basis.fish;
 
+import org.junit.Test;
+
 /**
  * @author caoyang
  */
@@ -9,5 +11,34 @@ public class LoopTest {
         for (int i = 0; i < young.length(); ++i) {
             System.out.println(young.getBytes()[i]);
         }
+
     }
+
+    @Test
+    public void test1() {
+        label:
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (j == 1) {
+                    break label;
+                }
+                System.out.println("j=" + j);
+            }
+        }
+    }
+
+    @Test
+    public void test2() {
+        label:
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (j == 1) {
+                    continue label;
+                }
+                System.out.println("j=" + j);
+            }
+        }
+
+    }
+
 }
